780adcce44eae9b44a7792cbfec466f24df212ec
Hirschen <hirsimaa@gmail.com>
2010-09-20 14:50:47 +0200

    added minor functions

--- Sokoban/State.java
+++ Sokoban/State.java
@@ -12,6 +12,10 @@ public class State{
 	public Box getBox(int i){
 		return boxes[i];
 	}
+	public void updateBox(int i, int x, int y){
+		boxes[i].xpos=x;
+		boxes[i].ypos=y;
+	}
 	public boolean isFree(int x, int y){
 		for(int i = 0; i < boxes.length; i++){
 			if(boxes[i].xpos == x && boxes[i].ypos == y){
