f28c66479333f3cd304a09db3ec398c7bf100eef
Agnes Skattman Udd <agnes@n147-p230.kthopen.kth.se>
2010-09-20 14:14:11 +0200

    updaterat box

--- Sokoban/Box.java
+++ Sokoban/Box.java
@@ -1,4 +1,20 @@
 
 public class Box {
 
+	int xpos;
+	int ypos;
+	
+	public Box(int xpos, int ypos) {
+		this.xpos = xpos;
+		this.ypos = ypos;
+		
+	}
+	
+	private int getX() {
+		return xpos;
+	}
+	
+	private int getY() {
+		return ypos;
+	}
 }
