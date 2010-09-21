aba4472f10198b98fe39caab9ef0a8401c5a1c21
Hirschen <hirsimaa@gmail.com>
2010-09-21 12:05:26 +0200

    bfsSolver with inf loops

--- Sokoban/Solver.java
+++ Sokoban/Solver.java
@@ -4,29 +4,40 @@ import java.util.LinkedList;
 import java.util.Map.Entry;
 
 public class Solver {
-	
-	private LinkedList<State> queue;
+
+	private static LinkedList<State> queue;
 
 	/**
 	 * 
 	 */
 	public static void main(String[] args) {
+		String mapString = "################\n" + "#@  $         .#\n"
+				+ "##  #          #\n" + "################\n";
+		Map map = Map.parse(mapString);
+
+		State state = new State(map.getStart(), map.getBoxes(), map);
 		
-		
+		bfsSolver(state);
 	}
-	public void bfsSolver(){
+
+	public static void bfsSolver(State start) {
 		queue = new LinkedList<State>();
+		queue.add(start);
 		State tmpState;
 		Box[] tmpBoxList;
-		while(!queue.isEmpty()){
+		while (!queue.isEmpty()) {
 			State curState = queue.poll();
-			for(Entry<Direction, Box> e : curState.getAvailableMoves()){
-				tmpBoxList = curState.getCopyBoxesWithNewMove(e.getValue().getPosition(),e.getKey());
-				tmpState = new State(e.getValue().getPosition(), tmpBoxList, curState.getMap() );
+			for (Entry<Direction, Box> e : curState.getAvailableMoves()) {
+				tmpBoxList = curState.getCopyBoxesWithNewMove(e.getValue()
+						.getPosition(), e.getKey());
+				tmpState = new State(e.getValue().getPosition(), tmpBoxList,
+						curState.getMap());
+				if (tmpState.goalReached()) {
+					System.out.println("WIN!");
+					return;
+				}
 				queue.add(tmpState);
 			}
 		}
 	}
 }
-
-	
